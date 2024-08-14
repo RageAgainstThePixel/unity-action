34bb3c260b380a2406b97343a11a3ebb642a7aef
Stephen Hodgson <hodgson.designs@gmail.com>
2024-08-14 12:46:16 -0400

    .

--- dist/index.js
+++ dist/index.js
@@ -26264,8 +26264,8 @@ async function ExecUnity(editorPath, args) {
     let exitCode = 0;
     switch (process.platform) {
         case 'linux':
-            core.info(`[command]${editorPath} ${args.join(' ')} -logFile ${logPath}`);
-            exitCode = await exec.exec(editorPath, [...args, `-logFile`, logPath], {
+            core.info(`[command]xvfb-run --auto-servernum ${editorPath} ${args.join(' ')} -logFile ${logPath}`);
+            exitCode = await exec.exec(`xvfb-run`, [`--auto-servernum`, `--server-args='-screen 0 640x480x24:32'`, editorPath, ...args, `-logFile`, logPath], {
                 listeners: {
                     stdline: (data) => {
                         const line = data.toString().trim();
